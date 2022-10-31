011e3d36ae2440235ddefb9a0a27dee7afecb71a
Teddy Ambona <44329177+teddy-ambona@users.noreply.github.com>
2022-10-31 21:56:44 +0100

    Rearranged config directory (#16)

--- app/config/gunicorn.py
+++ app/config/gunicorn.py
@@ -8,16 +8,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
 bind = "0.0.0.0:5000"
 
-# Write access and error info to /var/log
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
-
-# Redirect stdout/stderr to log file
-capture_output = True
-
-# PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/gunicorn.pid"
-
 # Explanation about the --timeout argument below
 # https://github.com/benoitc/gunicorn/issues/1801
 timeout = 1000
